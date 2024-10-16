8a8ddac7164f971e49271f7e6ec7db66fd02e63a
LolipopJ <42314340+LolipopJ@users.noreply.github.com>
2024-10-17 04:11:02 +0800

    chore(client): optimize month text

--- client/src/components/TimelineCurrentDate/index.tsx
+++ client/src/components/TimelineCurrentDate/index.tsx
@@ -9,7 +9,7 @@ interface TimelineCurrentDateProps {
 const NUMBER_ZH = ["〇", "壹", "貳", "叁", "肆", "伍", "陸", "柒", "捌", "玖"];
 const MONTH_ZH = [
   "寒至元月",
-  "暖冬二月",
+  "融冬二月",
   "陽春三月",
   "春和四月",
   "暖春五月",
@@ -18,7 +18,7 @@ const MONTH_ZH = [
   "風清八月",
   "秋高九月",
   "金秋十月",
-  "寒秋十一",
+  "悲秋十一",
   "歲末十二",
 ];
 const MONTH_COLOR = [
